e656ae626b77fba20641eb60029eb800dddefe97
3sk4p3 <skp85@hotmail.com>
2023-03-03 17:59:04 +0100

    finished Hashtable SKP

--- Source.cpp
+++ Source.cpp
@@ -1,102 +1,132 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <algorithm>
-
-
-
-std::vector<std::pair<long*, std::string*>>HashTable;
-std::string textIn;
-std::ifstream File;
+#include <string>
+#include <sstream>
+std::vector<std::pair<long, std::string>>HashTable;
+std::vector<std::pair<long,long>> OutOfOrder;//wanted,current
+std::vector<std::string> CompleteResult;
+std::string Result;
 int casesNo = 0;
 int ArrSize = 0;
 int SimpleHash(long _key) {
 	while (true)
 	{
-		if(HashTable[_key%ArrSize].first==NULL&& HashTable[_key % ArrSize].second == NULL)return _key % ArrSize;
+		if(HashTable[_key%ArrSize].first==NULL)return _key % ArrSize;
 
 		++_key;
 	}
 }
-void main()
+long DeleteFromMap(long _key)
 {
-	//HashTable.resize(5);
-	//HashTable[2] = { new long(2),new std::string("xddd")};
-	//HashTable[2] = { NULL ,NULL};
-	//for (size_t i = 0; i < 5; ++i)
-	//{
-	//	if (HashTable[i].first!=NULL&& HashTable[i].second != NULL)
-	//	{
-	//	std::cout << *HashTable[i].first << " " << *HashTable[i].second << std::endl;
-
-	//	}
-	//}
-
-	File.open("dane.txt");
-	if (File.is_open())
+	long idx = _key;
+	while (true)
 	{
-		File >> casesNo;
-		for (size_t i = 0; i < casesNo; ++i)
+		if (HashTable[idx % ArrSize].first == _key)
 		{
-		HashTable.clear();
-
-						while (true)
-						{
-
-							File >> textIn;
-							if (textIn == "stop")exit;
-							else if(textIn=="add")
-							{
-								int index = 0;
-								long _key = 0;
-								std::string  _value = "";
-								File >> _key >> _value;
-								index = SimpleHash(_key);
-								HashTable[index] = { new long(_key),new std::string(_value) };
-					
-							}
-							else if (textIn == "print")
-							{
-								for (size_t i = 0; i < ArrSize; ++i)
-								{
-									if (HashTable[i].first != NULL && HashTable[i].second != NULL) {
-									std::cout <<i<<" "<< * HashTable[i].first << " " << *HashTable[i].second << std::endl;
-									}
-								}
-								std::cout<<std::endl << std::endl;
-							}
-							else if (textIn == "delete")
-							{
-								long _key = 0;
-								File >> _key;
-
-								auto it = std::find_if(HashTable.begin(), HashTable.end(), [&](const std::pair<long*,std::string*> &pnt)
-									{	
-										if(pnt.first!=NULL)return *pnt.first == _key;
-											
-						
-									});
-								it->first = NULL;
-								it->second= NULL;
-								std::cout<<"first in delete: "<<it->first<<std::endl;
-								std::cout<<"second in delete: "<<it->second <<std::endl;
-
-							}
-							else if(textIn=="size")
-							{
-								File >> ArrSize;
-								HashTable.resize(ArrSize);
-							}
-
-						}
+			HashTable[idx % ArrSize].first = NULL;
+			break;
 		}
 
-
-		File.close();
+		++idx;
 	}
-	else std::cout << "nie udało się otworzyć pliku." << std::endl;
-	
+	return idx%ArrSize;
+}
+long Remap(const std::pair<long,long>&mp)
+{
+	int idx = 0;
+	idx = SimpleHash(HashTable[mp.second].first);
+	HashTable[idx].first = HashTable[mp.second].first;
+	HashTable[idx].second = HashTable[mp.second].second;
+	HashTable[mp.second].first = NULL;
+	return mp.second;
 
+}
+void main()
+{	
+	std::string command ;
+
+	std::cin >> casesNo;
+	CompleteResult.resize(casesNo);
+				std::cin.ignore();
+	for (size_t i = 0; i < casesNo; ++i)
+	{
+		Result.clear();
+		HashTable.clear();
+		
+		while (1)
+		{
+				getline(std::cin, command);
+				std::stringstream test(command);
+				if (command == "stop")break;
+			while (true)
+			{	
+				 std::string strval;
+				 
+				 if (!test)break;
+				 test >> strval;
+				 if (strval == "size")
+				 {
+					 test >> ArrSize;
+					 HashTable.resize(ArrSize);
+				 }
+				 else if (strval == "add")
+				 {	
+					 long key=0;
+					 std::string val;
+					 int idx = 0;
+					 test >> key;
+					 idx = SimpleHash(key);
+					 test >> val;
+					 if ((key%ArrSize) != idx)
+					 {
+						 OutOfOrder.push_back({ key%ArrSize,idx});
+					 }
+					 HashTable[idx] =std::make_pair(long(key),std::string(val));
+				 }
+				 else if (strval == "print")
+				 {
+					 int idx = 0;
+					 for (auto& i : HashTable)
+					 {
+						 if (i.first != NULL)Result += std::to_string(idx) + " " + std::to_string(i.first) + " " + i.second + "\n";
+						 ++idx;
+					 }
+					 Result += "\n";
+				 }
+
+				 else if (strval == "delete")
+				 {
+					 long key;
+					 test >> key;
+					 long freedIndex;
+					 freedIndex=DeleteFromMap(key);//index ktory sie wlasnie zwolnil
+					 for (auto& i : OutOfOrder)
+					 {
+						 if (i.first == freedIndex) {
+
+							 freedIndex=Remap(i);
+						 }
+					 }
+					
+					 
+				 }
+
+				 
+				 
+			}
+
+		
+			
+
+
+
+		}
+		CompleteResult.push_back(Result);
+	}
+	for (auto& i : CompleteResult)
+	{
+		std::cout << i << std::endl;
+	}
 
 
 	std::cin.get ();
